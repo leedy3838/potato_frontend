7e74208226ef9a40bc7e86edf436a3972feae9c1
LDY <leedy3838@naver.com>
2023-01-28 15:43:58 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from "react";
+import Navigation from "./components/nav";
+
+import "./App.css";
 
 function App() {
-   const [hello, setHello] = useState('')
-
-    useEffect(() => {
-        axios.get('/home')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
-
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
